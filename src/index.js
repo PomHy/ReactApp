f3fe11c36c3a50e8b4443dc5bea9b3aa030f6cec
Pontus Hylander <pontus.hylander@edu.edugrade.se>
2022-03-01 18:50:13 +0100

    Configure React app for deployment to GitHub Pages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import App2 from './App2';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App2 />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('mydiv')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+
+  function About () {
+    return (
+       <div>
+           <h2>Hi, I am a Car!</h2>;
+           <h2>Hi, I am a Car!</h2>;    
+       </div>
+   )
+ }
+export default About;
